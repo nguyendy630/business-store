741cf1c7cb65cd2fce5b2c912b844baf5b33f1a3
Dylan Nguyen <nguyendy630@hwcdsb.ca>
2024-01-10 12:32:14 -0500

    Added Hero Section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,11 +8,11 @@ export default function App() {
 
   useEffect(() => {
     axios.get('http://localhost:3001/getUsers')
-    .then(users => setUsers(users.data))
-    .catch(err => console.log(err))
+      .then(users => setUsers(users.data))
+      .catch(err => console.log(err))
   }, [])
 
-  return(
+  return (
     <>
       <header>
         <h1 className='logo'>Bon Cafe & Barber</h1>
@@ -20,14 +20,17 @@ export default function App() {
           <li><a href='#home'>Home</a></li>
           <li><a href='#about'>About</a></li>
           <li><a href='#services'>Services</a></li>
-          <li><button className="cta"><a href='#contact'>Contact</a></button></li>
+          <li><button className="cta">Contact</button></li>
         </ul>
       </header>
 
-      <section id='home'>
-        
+      <section id='hero'>
+        <div className="heading-text">
+          <h1>Best Coffee & Haircuts in Town</h1>
+          <button className="cta">Book Now</button>
+        </div>
       </section>
     </>
   );
-  
+
 }
