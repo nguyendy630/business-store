f6874f5e230722aee9dab208e5020028ec1dc0d6
nguyendy630 <nguyendylan2468@gmail.com>
2024-01-12 00:01:51 -0500

    Staged New Changes UI Update Animation on Scroll

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState, Route, Routes } from "react";
 import "./styles.css"
 import axios from "axios";
+import background from "./assets/background.jpg";
 
 export default function App() {
 
@@ -18,37 +19,27 @@ export default function App() {
 
   return (
     <>
-      <header>
-        <h1 className='logo'>Bon Cafe & Barber</h1>
-        <ul>
-          <li><a href='#home'>Home</a></li>
-          <li><a href='#about'>About</a></li>
-          <li><a href='#services'>Services</a></li>
-          <li><a href='#socials'>Socials</a></li>
-          <li><button className="cta" onClick={handleClick}>Contact</button></li>
+      <nav>
+        <div className="logo-nav">BON CAFE & <br/> BARBER</div>
+        <ul className="nav-links">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <button className="button">Contact</button>
         </ul>
-      </header>
+      </nav>
 
-      <section id='hero'>
-        <div className="heading-text">
-          <h1>Best Coffee & Haircuts in Town</h1>
-          <button className="cta">Book Now</button>
+      <section id="hero">
+        <div className="hero-text">
+          <div className='logo'></div>
         </div>
+        
       </section>
-
-      <section id='about'>
+      <section id="about">
         <div className="about-text">
-          <h2>About Us</h2>
+          
         </div>
       </section>
-
-      <section id="services">
-        <div className="services-text">
-          <h2>Our Services</h2>
-        </div>
-      </section>
-
     </>
   );
-
 }
